dba026b43b2872d23f140d63936f858732779324
RamenNoobles <rommel.angelov@gmail.com>
2020-04-03 17:12:23 -0700

    Update MasterScheduler.java

--- MasterScheduler.java
+++ MasterScheduler.java
@@ -108,7 +108,7 @@ class QuickStart {
 
     }
     public static void printElectives(){
-         System.out.println("\nElectives");
+        System.out.println("\nElectives");
         System.out.println("Period 1: " + ElectPeriod1);
         System.out.println("Period 2: " + ElectPeriod2);
         System.out.println("Period 3: " + AOEDPREP);
